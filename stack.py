fa559f442cab08cf24703b3a5f045834316d5381
OstapVando <ostapvando@gmail.com>
2022-09-15 19:09:19 +0300

    Add-method added to stack-class

--- stack.py
+++ stack.py
@@ -2,6 +2,24 @@ import sys
 
 class Stack():
     def __init__(self):
+        #stack object consists of two list 
+        #first one - in order that it was appended/poped
+        #secound - in order of element ascending
         self.items = []
-        self.max_value
-        self.max_
+        self.sorted =[]
+        self.lenght
+
+
+    #add - method that add a new value to the stack 
+    #and add a new value in sorted list in the right place
+    def add (self, data):
+        self.items.append(data)
+        self.sorted.append(data)
+        i = 2
+        while (data < self.sorted[-i]):
+            self.sorted[-(i-1)] = self.sorted[-i]
+            self.sorted[-i] = data
+            i += 1
+    
+
+    
