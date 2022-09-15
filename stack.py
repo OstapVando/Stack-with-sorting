99adf645c0a819931c4429f0b42cb3a37f5df887
OstapVando <ostapvando@gmail.com>
2022-09-15 19:31:07 +0300

    pop and max methods added

--- stack.py
+++ stack.py
@@ -7,8 +7,7 @@ class Stack():
         #secound - in order of element ascending
         self.items = []
         self.sorted =[]
-        self.lenght
-
+        self.lenght = 0
 
     #add - method that add a new value to the stack 
     #and add a new value in sorted list in the right place
@@ -20,6 +19,19 @@ class Stack():
             self.sorted[-(i-1)] = self.sorted[-i]
             self.sorted[-i] = data
             i += 1
-    
+        self.lenght += 1
+
+    #pop - method that delete last value from stack and the same value from sorted list
+    def pop (self):
+        data = self.items.pop()
+        i = 1
+        while (data != self.sorted[-i]):
+            i += 1
+        self.sorted.pop(-(i-1))
+        self.lenght -= 1
+
+    #Max - method that returns maximum value of stack
+    def max(self):
+        print(self.sorted[-1])
 
     
