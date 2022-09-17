c61f8a992c11ca3be8b2643e5264e5e683b490cb
OstapVando <ostapvando@gmail.com>
2022-09-18 00:23:01 +0300

    Final version: so simple - so fast

--- stack.py
+++ stack.py
@@ -5,20 +5,21 @@ class Stack():
     def __init__(self):
         self.items = np.array([0])
         self.sorted = np.array([0])
-        
+
+    #Add new element of stack    
     def Push (self, data):
         self.items = np.append(self.items, data)
         index = np.searchsorted(self.sorted, data)
         self.sorted = np.insert(arr=self.sorted, obj=index, values=data)
-        #print(self.items, self.sorted)
-
+        
+    #Delete last element of stack
     def Pop (self):
         data = self.items[-1]
         self.items = self.items[0:-1]
         index = np.searchsorted(self.sorted, data)
         self.sorted = np.concatenate((self.sorted[0:index], self.sorted[index+1::]))
-        #print(self.items, self.sorted)
-
+        
+    #Prints the greatest element of current stack
     def Max(self):
         print(self.sorted[-1])
 
